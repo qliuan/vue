1914969397fc96327bd435f518eaa3a333e19247
qliuan <qliuan@connect.ust.hk>
2018-04-08 15:45:25 -0400

    vuetify finished

---
+++ client/src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-title class="mr-4">
+      <v-btn flat dark
+        @click="navigateTo({name: 'root'})">
+        Atlanta Property Inventory
+      </v-btn>
+    </v-toolbar-title>
+
+    <!-- <v-toolbar-items>
+      <v-btn flat dark>
+        About
+      </v-btn>
+    </v-toolbar-items> -->
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn flat dark>
+        Visitor Sign Up
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-toolbar-items>
+      <v-btn flat dark>
+        Owner Sign Up
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-toolbar-items>
+      <v-btn flat dark
+        @click="navigateTo({name: 'register'})">
+        Login
+      </v-btn>
+    </v-toolbar-items>
+
+  </v-toolbar>
+</template>
+
+<script>
+// controller
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
