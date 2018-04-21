0fb6ef9a41e3560e625e2d6d742bbba6b9dcfa4f
qliuan <qliuan@connect.ust.hk>
2018-04-21 14:33:23 -0400

    change port to 9088

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8088/`
+    baseURL: `http://localhost:9088/`
     // headers: {
     //   Authorization: `Bearer ${store.state.token}`
     // }
