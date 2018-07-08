fdfc2a9343065bbd6f085c34f9e255b48faa8368
qliuan <qliuan@qliuans-MacBook-Pro.local>
2018-07-08 14:35:30 +0800

    change into local host

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,10 +1,20 @@
 var mysql = require('mysql')
+
+// Local Server
 var connect = mysql.createConnection({
-  host: 'academic-mysql.cc.gatech.edu',
-  user: 'cs4400_team_74',
-  password: 'S2h3p4Tr',
-  database: 'cs4400_team_74'
+  host: 'localhost',
+  user: 'root',
+  password: 'work_out@LQH',
+  database: 'cs4400gt'
 })
+
+// Remote Server
+// var connect = mysql.createConnection({
+//   host: 'academic-mysql.cc.gatech.edu',
+//   user: 'cs4400_team_74',
+//   password: 'S2h3p4Tr',
+//   database: 'cs4400_team_74'
+// })
 
 module.exports = {
   port: process.env.PORT || 9088,
